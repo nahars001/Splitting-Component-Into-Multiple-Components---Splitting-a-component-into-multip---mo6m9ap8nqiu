834bfd0309faa57194835e2706da1975deb1aa2d
Nahar Singh <naharsingh151299@gmail.com>
2023-01-11 22:59:53 +0530

    ds

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
         <div className="container">
             <Header id="header" content="This is the header"> </Header>
             <Main id="main" content= "This is the main"></Main>
-            <Footer id="footer" content ="This is the footer" > </Footer>
+            <Footer id="footer" content="This is the footer" >This is the footer</Footer>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer =(props)=> {
 return(
      <div id={props.id}>
         <footer id="footer">
-            {props.content}</footer>
+            {props.children}</footer>
      </div> 
 );
 }
