ac81eff0fc128a6fd5269defce04c46d9b869a5c
Nahar Singh <naharsingh151299@gmail.com>
2023-01-11 22:37:29 +0530

    s

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer =(props)=> {
 return(
-    <div id={props.id}>
+    <div id="footer">
         <footer id={props.id}>{props.content}</footer>
     </div>
 );
