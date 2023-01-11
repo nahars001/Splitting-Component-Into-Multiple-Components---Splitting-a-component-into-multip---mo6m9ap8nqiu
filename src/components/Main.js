5ffd3c8ebec0a79a806d6dbb5100bb305e3596b5
Nahar Singh <naharsingh151299@gmail.com>
2023-01-11 22:30:30 +0530

    s

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from "react";
 const Main = (props) => {
     return (
         <div id={props.id}>
-            {props.content}
+            <main id="container">{props.content}</main>
         </div>
     );
 }
