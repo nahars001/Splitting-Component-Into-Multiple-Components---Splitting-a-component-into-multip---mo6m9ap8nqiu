0d528b0c987d451dfa382ed604966bab983c79d6
Nahar Singh <naharsingh151299@gmail.com>
2023-01-11 22:49:10 +0530

    s

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 const Header = (props) => {
     return (
    <div id={props.id}> 
-            <header id="container">{props.content}</header>
+            <header id="my-header">{props.content}</header>
     </div>
     );
 }
