c92b6ec6f364146ece3d1cd82a50170d3b91c49d
Nahar Singh <naharsingh151299@gmail.com>
2023-01-11 23:01:43 +0530

    kj

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer =(props)=> {
 return(
      <div id={props.id}>
         <footer id="my-footer">
-            {props.children}</footer>
+            {props.content}</footer>
      </div> 
 );
 }
